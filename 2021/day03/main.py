c7ccef19281df6a7abdb1ac321ebafb629aa320f
Lorenz Lutz <lorenz@mklutz.de>
2021-12-03 14:36:14 +0100

    day 3 part 2

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -17,7 +17,7 @@ for i in range(bit_len):
             ones_minus_zeros += 1
         else:
             ones_minus_zeros -= 1
-    if ones_minus_zeros > 0:
+    if ones_minus_zeros >= 0:
         most_common_bits.append(1)
     else:
         most_common_bits.append(0)
@@ -30,3 +30,53 @@ epsilon_rate = gamma_rate ^ (2 ** bit_len - 1)
 power_consumption = gamma_rate * epsilon_rate
 
 print(f"part1: {power_consumption}")
+
+def find_most_common_bit(numbers, position):
+    ones_minus_zeros = 0
+    for number in numbers:
+        if (2 ** position) & number != 0:
+            ones_minus_zeros += 1
+        else:
+            ones_minus_zeros -= 1
+    return 1 if ones_minus_zeros >= 0 else 0
+
+oxygen_numbers = numbers
+
+for i in range(bit_len - 1, -1, -1):
+    criteria = find_most_common_bit(oxygen_numbers, i)
+    if criteria == 1:
+        oxygen_numbers = list(filter(lambda x: x & (2 ** i) != 0, oxygen_numbers))
+    else:
+        oxygen_numbers = list(filter(lambda x: x & (2 ** i) == 0, oxygen_numbers))
+
+    if len(oxygen_numbers) <= 1:
+        break
+
+oxygen_number = oxygen_numbers[0]
+
+def find_least_common_bit(numbers, position):
+    ones_minus_zeros = 0
+    for number in numbers:
+        if (2 ** position) & number != 0:
+            ones_minus_zeros += 1
+        else:
+            ones_minus_zeros -= 1
+    return 1 if ones_minus_zeros < 0 else 0
+
+co2_numbers = numbers
+
+for i in range(bit_len - 1, -1, -1):
+    criteria = find_least_common_bit(co2_numbers, i)
+    if criteria == 1:
+        co2_numbers = list(filter(lambda x: x & (2 ** i) != 0, co2_numbers))
+    else:
+        co2_numbers = list(filter(lambda x: x & (2 ** i) == 0, co2_numbers))
+
+    if len(co2_numbers) <= 1:
+        break
+
+co2_number = co2_numbers[0]
+
+life_support_rating = oxygen_number * co2_number
+
+print(f"part2: {life_support_rating}")
