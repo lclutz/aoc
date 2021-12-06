ff15b895982273f51143b9702a2bf9f0dbea8bd5
Lorenz Lutz <lorenz@mklutz.de>
2021-12-06 13:09:27 +0100

    day 22 part 2

--- 2020/day22/main.py
+++ 2020/day22/main.py
@@ -6,7 +6,7 @@ with open(ifile, mode="r") as f:
     deck1 = [int(i) for i in puzzle_input[1:player_split]]
     deck2 = [int(i) for i in puzzle_input[player_split + 2:]]
 
-history = list()
+history = set()
 
 
 def score(deck):
@@ -17,33 +17,72 @@ def score(deck):
     return s
 
 
-def play(deck1, deck2):
-    top_card_1, *deck1 = deck1
-    top_card_2, *deck2 = deck2
-    if top_card_1 > top_card_2:
-        deck1.append(top_card_1)
-        deck1.append(top_card_2)
-    else:
-        deck2.append(top_card_2)
-        deck2.append(top_card_1)
-    return (deck1, deck2)
-
-
 while len(deck1) > 0 and len(deck2) > 0:
-
-    if (deck1, deck2) in history:
+    if (tuple(deck1),tuple(deck2)) in history:
         # player 1 wins
         print(f"part1: {score(deck1)}")
         break
     else:
-        history.append((deck1,deck2))
-        deck1, deck2 = play(deck1, deck2)
-        if len(deck1) == 0:
-            print(f"part1: {score(deck2)}")
-            break
-        if len(deck2) == 0:
-            print(f"part1: {score(deck1)}")
-            break
+        history.add((tuple(deck1),tuple(deck2)))
+        card1 = deck1.pop(0)
+        card2 = deck2.pop(0)
+        if card1 > card2:
+            deck1 += [card1, card2]
+        else:
+            deck2 += [card2, card1]
 
 
+if len(deck1) == 0:
+    print(f"part1: {score(deck2)}")
+if len(deck2) == 0:
+    print(f"part1: {score(deck1)}")
 
+
+deck1 = [int(i) for i in puzzle_input[1:player_split]]
+deck2 = [int(i) for i in puzzle_input[player_split + 2:]]
+
+
+def recursive_combat(deck1, deck2):
+    card1 = deck1.pop(0)
+    card2 = deck2.pop(0)
+
+    if len(deck1) >= card1 and len(deck2) >= card2:
+        return recursive_combat(deck1[:card1], deck2[:card2])
+    else:
+        return 1 if card1 > card2 else 2
+
+
+class Game:
+
+    def __init__(self, deck1, deck2):
+        self.history = set()
+        self.deck1 = deck1
+        self.deck2 = deck2
+
+    def winner(self):
+        while len(self.deck1) != 0 and len(self.deck2) != 0:
+            if (tuple(self.deck1), tuple(self.deck2)) in self.history:
+                return 1
+            else:
+                self.history.add((tuple(self.deck1), tuple(self.deck2)))
+                card1 = self.deck1.pop(0)
+                card2 = self.deck2.pop(0)
+            if len(self.deck1) >= card1 and len(self.deck2) >= card2:
+                winner = Game(self.deck1[:card1], self.deck2[:card2]).winner()
+                if winner == 1:
+                    self.deck1 += [card1, card2]
+                else:
+                    self.deck2 += [card2, card1]
+            else:
+                if card1 > card2:
+                    self.deck1 += [card1, card2]
+                else:
+                    self.deck2 += [card2, card1]
+        return 1 if len(self.deck2) == 0 else 2
+
+
+g = Game(deck1, deck2)
+if g.winner() == 1:
+    print(f"part2: {score(g.deck1)}")
+else:
+    print(f"part2: {score(g.deck2)}")
