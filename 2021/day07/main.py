c787adca2591470a1fb167b1a41f3b0aeedfa7c0
Lorenz Lutz <lorenz@mklutz.de>
2021-12-07 21:37:55 +0100

    day 7 part 2

--- 2021/day07/main.py
+++ 2021/day07/main.py
@@ -9,3 +9,10 @@ for p in range(max(puzzle_input)):
     costs.append(cost)
 
 print(f"part1: {min(costs)}")
+
+costs = list()
+for p in range(max(puzzle_input)):
+    cost = sum([sum(range(abs(i - p) + 1)) for i in puzzle_input])
+    costs.append(cost)
+
+print(f"part2: {min(costs)}")
