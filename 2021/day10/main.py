404906e436b34cd36474f34ac25e3a0253a9fd9b
Lorenz Lutz <lorenz@mklutz.de>
2021-12-11 10:54:00 +0100

    day 10 part 2

--- 2021/day10/main.py
+++ 2021/day10/main.py
@@ -37,3 +37,37 @@ for line_number, line in enumerate(puzzle_input):
                 break
 
 print(f"part1: {syntax_error_score}")
+
+scoring_table = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+autocomplete_scores = list()
+
+for line_number in range(len(puzzle_input)):
+    if line_number in corruped_lines:
+        continue
+
+    line = puzzle_input[line_number]
+    total_score = 0
+
+    stack = list()
+    for paran in line:
+        if paran in openings:
+            stack.append(paran)
+        elif paran in closings:
+            stack.pop()
+
+    stack.reverse()
+    for c in stack:
+        total_score *= 5
+        total_score += scoring_table[open_close[c]]
+    autocomplete_scores.append(total_score)
+
+autocomplete_scores.sort()
+middle_score = autocomplete_scores[len(autocomplete_scores) // 2]
+
+print(f"part2: {middle_score}")
